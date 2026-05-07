68f78ae6dac4604d7a50a458f6d8a93d95ebd977
Leonel Ornelas <lornelas.29@outlook.com>
2026-05-07 00:40:31 -0600

    Subimos_batas_de_colores

--- src/lib/products-store.ts
+++ src/lib/products-store.ts
@@ -117,6 +117,24 @@ const defaultProducts: Product[] = [
     inStock: true, featured: true, isPromo: false, ratings: [],
   },
 
+          {
+    id: '9', name: 'Bata con capucha | Ropa de baño para mascotas',
+    description: `Mantén a tu mascota calientita, cómoda y protegida del frío con nuestra toalla de baño con capucha, suave y de alta calidad`,
+    price: 339, category: 'ropa', sizes: ['0','1','2','3','4','5','6'],
+    stock: [{size:'0',qty:1},{size:'1',qty:100},{size:'2',qty:100},{size:'3',qty:5},{size:'4',qty:2},{size:'5',qty:5},{size:'6',qty:2},{size:'7',qty:2}],  
+    images: ['https://i.ibb.co/jZxY0nCb/Bataroja.jpg'],
+    inStock: true, featured: true, isPromo: false, ratings: [],
+  },
+
+          {
+    id: '10', name: 'Bata con capucha | Ropa de baño para mascotas',
+    description: `Mantén a tu mascota calientita, cómoda y protegida del frío con nuestra toalla de baño con capucha, suave y de alta calidad`,
+    price: 339, category: 'ropa', sizes: ['0','1','2','3','4','5','6'],
+    stock: [{size:'0',qty:1},{size:'1',qty:100},{size:'2',qty:100},{size:'3',qty:5},{size:'4',qty:2},{size:'5',qty:5},{size:'6',qty:2},{size:'7',qty:2}],  
+    images: ['https://i.ibb.co/Y4bTgjJS/Batarosa.jpg'],
+    inStock: true, featured: true, isPromo: false, ratings: [],
+  },
+
 ];
 
 function readProductsExtra(): Product[] {
